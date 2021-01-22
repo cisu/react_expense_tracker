9bb34b868670a34438f9dd1b841b8f2fe8af0b5d
cisu <49948070+cisu@users.noreply.github.com>
2021-01-22 18:38:50 +0200

    add globalState transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,8 +1,9 @@
 import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
 
 //  Initial state
 const initialState = {
-  transaction: [
+  transactions: [
     {id: 1, text: 'Flower', amount: -20},
     {id: 2, text: 'Salary', amount: 300},
     {id: 3, text: 'Book', amount: -10},
@@ -17,5 +18,13 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({children}) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
